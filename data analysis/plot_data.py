dd437f49f7939fee5f071ced2c3eda506d1789d0
Eivind Lie Andreassen <ela4991@gmail.com>
2019-03-11 12:16:19 +0100

    Style plot with seaborn

--- data analysis/plot_data.py
+++ data analysis/plot_data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+sns.set_context('paper')
+sns.set_palette('muted')
 
 data = pd.read_csv('data/data.csv', index_col=0, parse_dates=True)
 
