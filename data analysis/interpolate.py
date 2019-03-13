3744c0dd5f46003342bbb25720117eb4c2036e12
Eivind Lie Andreassen <ela4991@gmail.com>
2019-03-13 10:50:54 +0100

    Resample to 1m, 5m, and 10m

--- data analysis/interpolate.py
+++ data analysis/interpolate.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('data/data.csv', parse_dates=True, index_col=[0])
 
-resample = data.resample('10T')
-resample = resample.mean()
+resample_1m = data.resample('1T')
+resample_1m = resample_1m.mean()
+resample_1m.to_csv('data/data-1m.csv')
 
-resample.to_csv('data/data-10m.csv')
+resample_5m = data.resample('5T')
+resample_5m = resample_5m.mean()
+resample_5m.to_csv('data/data-5m.csv')
+
+resample_10m = data.resample('10T')
+resample_10m = resample_10m.mean()
+resample_10m.to_csv('data/data-10m.csv')
